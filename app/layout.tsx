e4718291f4399267dfabcbc17309f39f599790c0
Valentina.Baranova <valentinab@bluetrailsoft.com>
2025-11-18 13:19:12 -0300

    Added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import { SessionProvider } from "next-auth/react";
 import NavBar from '@/ui/navbar';
 import { I18nProvider } from '@/i18n/I18nProvider';
 import AntdLocaleProvider from '@/antd/AntdLocaleProvider';
+import Script from 'next/script';
+
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 export default function RootLayout({
    children,
@@ -14,6 +17,21 @@ export default function RootLayout({
     <SessionProvider>
         <html lang="es">
         <body>
+        {/* Google Analytics 4 */}
+        {GA_MEASUREMENT_ID ? (
+            <>
+                <Script
+                    src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+                    strategy="afterInteractive"
+                />
+                <Script id="ga4-init" strategy="afterInteractive">{`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);} 
+                    gtag('js', new Date());
+                    gtag('config', '${GA_MEASUREMENT_ID}');
+                `}</Script>
+            </>
+        ) : null}
         <I18nProvider>
             <AntdLocaleProvider>
                 <NavBar />
